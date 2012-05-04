30c1699186b73bb6e56253d78a651cd61ff9a5bd
mamund <mamund@yahoo.com>
2012-05-04 19:21:26 +0000

    use str

--- app.js
+++ app.js
@@ -3,7 +3,8 @@
 var http = require('http');
 
 function handler(req, res) {
+    var str = '<h1>Hello, StirTrek from ' + req.url + '</h1>';
     res.writeHead(200, 'OK', {'content-type' : 'text/html'});
-    res.end('<h1>Hello, StirTrek from ' + req.url + '</h1>');
+    res.end(str);
 }
 http.createServer(handler).listen(process.env.PORT || 1337);
